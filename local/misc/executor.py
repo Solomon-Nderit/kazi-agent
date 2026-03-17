8b4902ca43541ae4f9e1b982814cabd4e8238b5c
Solomon Nderitu Wahome <92320793+Solomon-Nderit@users.noreply.github.com>
2026-03-17 17:40:09 +0300

    cleaned up page

--- local/misc/executor.py
+++
@@ -1,5 +0,0 @@
-from ..action import take_action
-
-# Simple wrapper if you want to abstract it further later.
-def perform_action(action_dict, target=None, value=None):
-    pass
--- main.py
+++
@@ -1,4 +0,0 @@
-from local.misc.screenshot import get_screen_with_grid
-from local.action import execute_actions
-
-
